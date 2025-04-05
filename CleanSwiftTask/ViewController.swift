9d43052e31eaa1c459caa49620b029ba7d17ef7f
Anton Ivchenko <kvadrostoplerman@gmail.com>
2025-04-05 15:04:15 +0300

    Initial Commit

---
+++ CleanSwiftTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanSwiftTask
+//
+//  Created by Anton Ivchenko on 05.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
